c232d9b6e730a172f09500ca6bd679f28353b45a
RoboWeb <RoboWeb@users.noreply.github.com>
2020-06-12 08:49:19 +0200

    hide secrets

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,13 +3,13 @@ import firestore from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA7Ds7gCbRGNOt2-iZUBZB1A4lMmTFS4jI',
+  apiKey: '[api-key]',
   authDomain: 'wmapps-81e45.firebaseapp.com',
-  databaseURL: 'https://wmapps-81e45.firebaseio.com',
-  projectId: 'wmapps-81e45',
-  storageBucket: 'wmapps-81e45.appspot.com',
+  databaseURL: 'https://[project-id].firebaseio.com',
+  projectId: '[project-id]',
+  storageBucket: '[project-id].appspot.com',
   messagingSenderId: '566980532191',
-  appId: '1:566980532191:web:1d1bddf44541e904c7166a',
+  appId: '[app-id]',
   measurementId: 'G-WEHNTYPDE7'
 };
 // Initialize Firebase
